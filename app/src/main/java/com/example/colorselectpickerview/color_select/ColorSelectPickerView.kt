3ed38787fc5bfb41f68a32632d4fc309c73ca59e
lyd <liyandong@nn.com>
2024-01-09 18:31:28 +0800

    优化一部分视图，从原先xml方式改为View绘制，更简洁一些

--- app/src/main/java/com/example/colorselectpickerview/color_select/ColorSelectPickerView.kt
+++ app/src/main/java/com/example/colorselectpickerview/color_select/ColorSelectPickerView.kt
@@ -3,14 +3,13 @@ package com.example.colorselectpickerview.color_select
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.widget.FrameLayout
-import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.annotation.ColorInt
 import com.example.colorselectpickerview.R
 
 
@@ -19,111 +18,111 @@ import com.example.colorselectpickerview.R
  */
 class ColorSelectPickerView : LinearLayout {
 
-    private lateinit var llColorProgress: View
-    private lateinit var vColorBar: View
-    private lateinit var rlTransBar: View
-    private lateinit var vTransBar: View
-    private lateinit var vLocation: View
+    private lateinit var colorView: RainbowColorBarView //彩虹条View
+    private lateinit var colorBar: View //彩虹条index
+
+    private lateinit var alphaView: RainbowColorBarView //透明度View
+    private lateinit var alphaBar: View //透明度index
+
+    private lateinit var colorBoardBar: View //明暗度index
     private lateinit var vBgColor: View
-    private lateinit var vTransPreview: ImageView
 
-    private lateinit var colorBarLayoutParams: FrameLayout.LayoutParams
-    private lateinit var transBarLayoutParams: FrameLayout.LayoutParams
-    private lateinit var vLocationLayoutParams: FrameLayout.LayoutParams
+    private var previewBar: RainbowColorBarView? = null
 
-    private fun isInitialized(): Boolean {
-        return this::llColorProgress.isInitialized &&
-                this::vColorBar.isInitialized &&
-                this::rlTransBar.isInitialized &&
-                this::vTransBar.isInitialized &&
-                this::vLocation.isInitialized &&
-                this::vBgColor.isInitialized &&
-                this::vTransPreview.isInitialized &&
-                this::colorBarLayoutParams.isInitialized &&
-                this::transBarLayoutParams.isInitialized &&
-                this::vLocationLayoutParams.isInitialized
-    }
+    private lateinit var colorSelectBarLP: FrameLayout.LayoutParams
+    private lateinit var alphaSelectBarLP: FrameLayout.LayoutParams
+    private lateinit var colorBoardLP: FrameLayout.LayoutParams
 
-    private var red = 255
-    private var green: Int = 0
-    private var blue: Int = 0
+    /*当前彩虹条 rgb*/
+    private var colorRed = 255
+    private var colorGreen: Int = 0
+    private var colorBlue: Int = 0
+
+    /*选择的rgb*/
+    private var indexRed = 255
+    private var indexGreen: Int = 0
+    private var indexBlue: Int = 0
+
+    /*透明度*/
+    private var indexAlpha = 255
+
     private var index = 0
-    private var cpvColorPreview: ColorSelectPreviewView? = null
-    private var transValue = 255 //透明度
 
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        if (isInEditMode) return
+        //if (isInEditMode) return
         initView()
     }
 
-    @SuppressLint("ClickableViewAccessibility")
     private fun initView() {
         val view = LayoutInflater.from(context).inflate(R.layout.view_color_picker, this)
+        /*明暗度*/
         vBgColor = view.findViewById(R.id.fl_color)
-        vLocation = view.findViewById(R.id.view_location)
-        vLocationLayoutParams = vLocation.layoutParams as FrameLayout.LayoutParams
+        colorBoardBar = view.findViewById(R.id.view_location)
+        colorBoardLP = colorBoardBar.layoutParams as FrameLayout.LayoutParams
 
-        llColorProgress = findViewById(R.id.ll_color_progress)
-        cpvColorPreview = view.findViewById(R.id.cpv_color_preview)
-        vColorBar = view.findViewById(R.id.view_color_bar)
-        colorBarLayoutParams = vColorBar.layoutParams as FrameLayout.LayoutParams
+        /*预览*/
+        previewBar = view.findViewById(R.id.preview_color)
 
-        rlTransBar = view.findViewById(R.id.rl_trans_bar)
-        vTransBar = view.findViewById(R.id.view_trans_bar)
-        transBarLayoutParams = vTransBar.layoutParams as FrameLayout.LayoutParams
-        vTransPreview = view.findViewById(R.id.view_trans_preview)
+        /*彩虹条*/
+        colorView = findViewById(R.id.view_color)
+        colorBar = view.findViewById(R.id.view_select_color_bar)
+        colorSelectBarLP = colorBar.layoutParams as FrameLayout.LayoutParams
 
-        /*调整颜色*/
-        llColorProgress.setOnTouchListener { _, event ->
-            val width = llColorProgress.width
+        /*透明度*/
+        alphaView = view.findViewById(R.id.view_alpha)
+        alphaBar = view.findViewById(R.id.view_select_alpha_bar)
+        alphaSelectBarLP = alphaBar.layoutParams as FrameLayout.LayoutParams
+
+        layoutTouch()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun layoutTouch() {
+        /*调整彩虹条颜色*/
+        colorView.setOnTouchListener { _, event ->
+            val width = colorView.width
             when (event.action) {
-                MotionEvent.ACTION_DOWN -> {}
-                MotionEvent.ACTION_MOVE -> {}
-                MotionEvent.ACTION_UP -> {}
+                MotionEvent.ACTION_DOWN, MotionEvent.ACTION_UP, MotionEvent.ACTION_MOVE -> {}
             }
             val leftMargin = event.x
             var x = 0f
-            if (leftMargin < vColorBar.width / 2.0f) {
-                colorBarLayoutParams.leftMargin = 0
-            } else if (leftMargin > width - vColorBar.width / 2.0f) {
+            if (leftMargin < colorBar.width / 2.0f) {
+                colorSelectBarLP.leftMargin = 0
+            } else if (leftMargin > width - colorBar.width / 2.0f) {
                 x = 100f
-                colorBarLayoutParams.leftMargin = width - vColorBar.width
+                colorSelectBarLP.leftMargin = width - colorBar.width
             } else {
                 x = event.x / width * 100
-                colorBarLayoutParams.leftMargin = (leftMargin - vColorBar.width / 2).toInt()
+                colorSelectBarLP.leftMargin = (leftMargin - colorBar.width / 2).toInt()
             }
-            vColorBar.layoutParams = colorBarLayoutParams
-            onProgressChanged(x.toInt())
+            colorBar.layoutParams = colorSelectBarLP
+            changedColorBar(x.toInt())
             true
         }
 
         /*调整透明度*/
-
-        rlTransBar.setOnTouchListener { _, event ->
-            val width = rlTransBar.width
+        alphaView.setOnTouchListener { _, event ->
+            val width = alphaView.width
             when (event.action) {
-                MotionEvent.ACTION_DOWN -> {}
-                MotionEvent.ACTION_MOVE -> {}
-                MotionEvent.ACTION_UP -> {}
+                MotionEvent.ACTION_DOWN, MotionEvent.ACTION_UP, MotionEvent.ACTION_MOVE -> {}
             }
             val leftMargin = event.x
             var x = 0f
-            if (leftMargin < vTransBar.width / 2.0f) {
-                transBarLayoutParams.leftMargin = 0
-            } else if (leftMargin > width - vTransBar.width / 2.0f) {
+            if (leftMargin < alphaBar.width / 2.0f) {
+                alphaSelectBarLP.leftMargin = 0
+            } else if (leftMargin > width - alphaBar.width / 2.0f) {
                 x = 100f
-                transBarLayoutParams.leftMargin = width - vTransBar.width
+                alphaSelectBarLP.leftMargin = width - alphaBar.width
             } else {
                 x = event.x / width * 100
-                transBarLayoutParams.leftMargin = (leftMargin - vTransBar.width / 2).toInt()
+                alphaSelectBarLP.leftMargin = (leftMargin - alphaBar.width / 2).toInt()
             }
-            vTransBar.layoutParams = transBarLayoutParams
-            changeTransparency(x.toInt())
+            alphaBar.layoutParams = alphaSelectBarLP
+            changeAlpha(x.toInt())
             true
         }
-
 
         /*调整颜色明暗*/
         vBgColor.setOnTouchListener { _, event ->
@@ -132,29 +131,28 @@ class ColorSelectPickerView : LinearLayout {
             val leftMargin: Int
             val topMargin: Int
             when (event.action) {
-                MotionEvent.ACTION_DOWN -> {}
+                MotionEvent.ACTION_DOWN, MotionEvent.ACTION_UP -> {}
                 MotionEvent.ACTION_MOVE -> {
                     //防止越界处理
-                    leftMargin = if (event.x > width - vLocation.width / 2f) {
-                        width - vLocation.width
-                    } else if (event.x < vLocation.width / 2f) {
+                    leftMargin = if (event.x > width - colorBoardBar.width / 2f) {
+                        width - colorBoardBar.width
+                    } else if (event.x < colorBoardBar.width / 2f) {
                         0
                     } else {
-                        (event.x - vLocation.width / 2f).toInt()
+                        (event.x - colorBoardBar.width / 2f).toInt()
                     }
-                    topMargin = if (event.y > height - vLocation.height / 2f) {
-                        height - vLocation.height
-                    } else if (event.y <= vLocation.height / 2f) {
+                    topMargin = if (event.y > height - colorBoardBar.height / 2f) {
+                        height - colorBoardBar.height
+                    } else if (event.y <= colorBoardBar.height / 2f) {
                         0
                     } else {
-                        (event.y - vLocation.height / 2f).toInt()
+                        (event.y - colorBoardBar.height / 2f).toInt()
                     }
-                    vLocationLayoutParams.leftMargin = leftMargin
-                    vLocationLayoutParams.topMargin = topMargin
-                    vLocation.layoutParams = vLocationLayoutParams
-                    changeColor()
+                    colorBoardLP.leftMargin = leftMargin
+                    colorBoardLP.topMargin = topMargin
+                    colorBoardBar.layoutParams = colorBoardLP
+                    changeColorBoard()
                 }
-                MotionEvent.ACTION_UP -> {}
             }
             true
         }
@@ -163,132 +161,134 @@ class ColorSelectPickerView : LinearLayout {
     /**
      * 颜色值调整
      */
-    private fun onProgressChanged(progressColor: Int) {
-        red = 0
-        green = 0
-        blue = 0
+    private fun changedColorBar(progressColor: Int) {
+        colorRed = 0
+        colorGreen = 0
+        colorBlue = 0
         index = (progressColor / (100 / 6f)).toInt()
         val v = progressColor % (100 / 6f) / (100 / 6f)
         when (index) {
             0 -> {
-                red = 255
-                green = (255 * v).toInt()
+                colorRed = 255
+                colorGreen = (255 * v).toInt()
             }
 
             1 -> {
-                red = (255 * (1 - v)).toInt()
-                green = 255
+                colorRed = (255 * (1 - v)).toInt()
+                colorGreen = 255
             }
 
             2 -> {
-                green = 255
-                blue = (255 * v).toInt()
+                colorGreen = 255
+                colorBlue = (255 * v).toInt()
             }
 
             3 -> {
-                green = (255 * (1 - v)).toInt()
-                blue = 255
+                colorGreen = (255 * (1 - v)).toInt()
+                colorBlue = 255
             }
 
             4 -> {
-                blue = 255
-                red = (255 * v).toInt()
+                colorBlue = 255
+                colorRed = (255 * v).toInt()
             }
 
             5 -> {
-                blue = (255 * (1 - v)).toInt()
-                red = 255
+                colorBlue = (255 * (1 - v)).toInt()
+                colorRed = 255
             }
 
-            else -> red = 255
+            else -> colorRed = 255
         }
-        vBgColor.setBackgroundColor(Color.rgb(red, green, blue))
-        changeColor()
+        vBgColor.setBackgroundColor(Color.rgb(colorRed, colorGreen, colorBlue))
+        changeColorBoard()
     }
 
     /**
      * 颜色明暗度调整
      */
-    private fun changeColor() {
-        var tempRed = red
-        var tempGreen = green
-        var tempBlue = blue
-        val hPercent = 1 - vLocation.x / (vBgColor.width - vLocation.width)
-        val vPercent = vLocation.y / (vBgColor.height - vLocation.height)
+    private fun changeColorBoard() {
+        var tempRed = colorRed
+        var tempGreen = colorGreen
+        var tempBlue = colorBlue
+        val xPercent = 1 - colorBoardBar.x / (vBgColor.width - colorBoardBar.width)
+        val yPercent = colorBoardBar.y / (vBgColor.height - colorBoardBar.height)
         when (index) {
-            0 -> {
-                tempGreen = (green + hPercent * (255 - green)).toInt()
-                tempBlue = (blue + hPercent * (255 - blue)).toInt()
+            0, 5, 6 -> {
+                tempGreen = (colorGreen + xPercent * (255 - colorGreen)).toInt()
+                tempBlue = (colorBlue + xPercent * (255 - colorBlue)).toInt()
             }
 
-            1 -> {
-                tempRed = (red + hPercent * (255 - red)).toInt()
-                tempBlue = (blue + hPercent * (255 - blue)).toInt()
+            1, 2 -> {
+                tempRed = (colorRed + xPercent * (255 - colorRed)).toInt()
+                tempBlue = (colorBlue + xPercent * (255 - colorBlue)).toInt()
             }
 
-            2 -> {
-                tempRed = (red + hPercent * (255 - red)).toInt()
-                tempBlue = (blue + hPercent * (255 - blue)).toInt()
-            }
-
-            3 -> {
-                tempRed = (red + hPercent * (255 - red)).toInt()
-                tempGreen = (green + hPercent * (255 - green)).toInt()
-            }
-
-            4 -> {
-                tempRed = (red + hPercent * (255 - red)).toInt()
-                tempGreen = (green + hPercent * (255 - green)).toInt()
-            }
-
-            5, 6 -> {
-                tempGreen = (green + hPercent * (255 - green)).toInt()
-                tempBlue = (blue + hPercent * (255 - blue)).toInt()
+            3, 4 -> {
+                tempRed = (colorRed + xPercent * (255 - colorRed)).toInt()
+                tempGreen = (colorGreen + xPercent * (255 - colorGreen)).toInt()
             }
         }
-        tempRed = (tempRed - tempRed * vPercent).toInt()
-        tempGreen = (tempGreen - tempGreen * vPercent).toInt()
-        tempBlue = (tempBlue - tempBlue * vPercent).toInt()
-        val color = Color.argb(transValue, tempRed, tempGreen, tempBlue)
-        cpvColorPreview?.setColor(color)
-        colorChanged?.invoke(color)
+        indexRed = (tempRed - tempRed * yPercent).toInt()
+        indexGreen = (tempGreen - tempGreen * yPercent).toInt()
+        indexBlue = (tempBlue - tempBlue * yPercent).toInt()
 
-        val gradientColor = intArrayOf(Color.argb(0, 0, 0, 0), Color.rgb(tempRed, tempGreen, tempBlue))
-        val drawable = GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, gradientColor)
-        vTransPreview.background = drawable
+        val color = Color.argb(indexAlpha, indexRed, indexGreen, indexBlue)
+        previewBar?.setPreviewColor(color)
+        colorChanged?.invoke(color)
+        setAlphaRgb(color)
     }
 
     /**
      * 透明度
      */
-    private fun changeTransparency(progress: Int) {
-        transValue = (progress / 100f * 255).toInt()
-        val color = Color.argb(transValue, red, green, blue)
-        cpvColorPreview?.setColor(color)
+    private fun changeAlpha(progress: Int) {
+        indexAlpha = (progress / 100f * 255).toInt()
+        val color = Color.argb(indexAlpha, indexRed, indexGreen, indexBlue)
+        previewBar?.setPreviewColor(color)
         colorChanged?.invoke(color)
+    }
+
+    /**
+     * 设置透明度Bar色值
+     */
+    private fun setAlphaRgb(@ColorInt rgbColor: Int? = null) {
+        if (rgbColor == null) {
+            alphaView.setAlphaColor(Color.rgb(255, 0, 0))
+            return
+        }
+        alphaView.setAlphaColor(rgbColor)
     }
 
     override fun onWindowFocusChanged(hasWindowFocus: Boolean) {
         super.onWindowFocusChanged(hasWindowFocus)
-        if (!isInitialized()) return
-        val layoutParams = vLocation.layoutParams as FrameLayout.LayoutParams
-        layoutParams.leftMargin = vBgColor.width - vLocation.width
-        vLocation.layoutParams = layoutParams
 
-        colorBarLayoutParams.leftMargin = llColorProgress.width - vColorBar.width
-        vColorBar.layoutParams = colorBarLayoutParams
+        val layoutParams = colorBoardBar.layoutParams as FrameLayout.LayoutParams
+        layoutParams.leftMargin = initBarPosition(BarIndex.End, vBgColor, colorBoardBar)
+        colorBoardBar.layoutParams = layoutParams
 
-        transBarLayoutParams.leftMargin = rlTransBar.width - vTransBar.width
-        vTransBar.layoutParams = transBarLayoutParams
+        colorSelectBarLP.leftMargin = initBarPosition(BarIndex.Start, colorView, colorBar)
+        colorBar.layoutParams = colorSelectBarLP
 
-        val color = intArrayOf(Color.argb(0, 0, 0, 0), Color.rgb(255, 0, 0))
-        val drawable = GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, color)
-        vTransPreview.background = drawable
+        alphaSelectBarLP.leftMargin = initBarPosition(BarIndex.End, alphaView, alphaBar)
+        alphaBar.layoutParams = alphaSelectBarLP
+
+        setAlphaRgb()
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
         parent.requestDisallowInterceptTouchEvent(true)
         return super.dispatchTouchEvent(ev)
+    }
+
+    private fun initBarPosition(barIndex: BarIndex, parentView: View, barView: View): Int = when (barIndex) {
+        BarIndex.Start -> 0
+        BarIndex.Centre -> (parentView.width / 2) - (barView.width / 2)
+        BarIndex.End -> parentView.width - barView.width
+    }
+
+    private enum class BarIndex {
+        Start, Centre, End
     }
 
     var colorChanged: ((Int) -> Unit)? = null
---
+++ app/src/main/java/com/example/colorselectpickerview/color_select/RainbowColorBarView.kt
@@ -0,0 +1,148 @@
+package com.example.colorselectpickerview.color_select
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.BitmapShader
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Shader
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import androidx.annotation.ColorInt
+import com.example.colorselectpickerview.R
+
+/**
+ *
+ * @author lyd
+ * @time 2024/1/9 09:46:46
+ */
+class RainbowColorBarView : View {
+
+    private var paint: Paint? = null
+    private var srcPaint: Paint? = null
+
+    private var rectF: RectF? = null
+    private var canvasRadius: Float = 0f //半径
+    private var canvasType: Int = 0
+    private var isPreviewCircle: Boolean = false
+
+    private var colors: IntArray? = null
+    private var positions: FloatArray? = null
+
+    private val alphaBitmap by lazy {
+        BitmapFactory.decodeResource(resources, R.drawable.color_select_bg_trans_01)
+    }
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        //if (isInEditMode) return
+        initView(context, attrs)
+    }
+
+    private fun initView(context: Context, attrs: AttributeSet?) {
+        val typeArray = context.obtainStyledAttributes(attrs, R.styleable.RainbowColorBarView)
+        canvasRadius = typeArray.getDimension(R.styleable.RainbowColorBarView_round, 0f)
+        canvasType = typeArray.getInt(R.styleable.RainbowColorBarView_graph_type, 1)
+        canvasType = typeArray.getInt(R.styleable.RainbowColorBarView_graph_type, 1)
+        isPreviewCircle = typeArray.getBoolean(R.styleable.RainbowColorBarView_preview_circle, false)
+        typeArray.recycle()
+        paint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.DITHER_FLAG)
+        rectF = RectF()
+        paint?.isAntiAlias = true
+        paint?.style = Paint.Style.FILL
+
+        when (canvasType) {
+            1 -> {
+                colors = intArrayOf(Color.RED, Color.YELLOW, Color.GREEN, Color.CYAN, Color.BLUE, Color.MAGENTA, Color.RED)
+                positions = floatArrayOf(0f, 1.0f / 6, 2.0f / 6, 3.0f / 6, 4.0f / 6, 5.0f / 6, 1f)
+            }
+
+            2 -> {
+                colors = intArrayOf(Color.TRANSPARENT, Color.RED)
+                positions = floatArrayOf(0f, 1f)
+                srcPaint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.DITHER_FLAG)
+                srcPaint?.isAntiAlias = true
+                srcPaint?.shader = BitmapShader(alphaBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+            }
+
+            3 -> {
+                paint?.color = Color.RED
+                srcPaint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.DITHER_FLAG)
+                srcPaint?.isAntiAlias = true
+                srcPaint?.shader = BitmapShader(alphaBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+            }
+        }
+        setWillNotDraw(false)
+    }
+
+    /**
+     * 设置透明度
+     *
+     * @param rgbColor
+     */
+    fun setAlphaColor(@ColorInt rgbColor: Int) {
+        if (canvasType != 2 && colors?.isNotEmpty() == true && colors?.size == 2) return
+        colors!![1] = rgbColor
+        paint?.shader = LinearGradient(0f, 0f, width.toFloat(), 0f, colors!!, positions, Shader.TileMode.CLAMP)
+        invalidate()
+    }
+
+    /**
+     * 设置预览颜色
+     *
+     * @param previewColor
+     */
+    fun setPreviewColor(@ColorInt previewColor: Int) {
+        if (canvasType != 3) return
+        paint?.color = previewColor
+        invalidate()
+    }
+
+    @SuppressLint("DrawAllocation")
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        if (rectF == null || paint == null) return
+        when (canvasType) {
+            1, 2 -> {
+                if (colors == null || positions == null) return
+                val gradient = LinearGradient(0f, 0f, width.toFloat(), 0f, colors!!, positions, Shader.TileMode.CLAMP)
+                paint?.shader = gradient
+                if (canvasType == 2) {
+                    srcPaint?.let { canvas.drawRoundRect(rectF!!, canvasRadius, canvasRadius, it) }
+                }
+                canvas.drawRoundRect(rectF!!, canvasRadius, canvasRadius, paint!!)
+            }
+
+            3 -> {
+                paint?.shader = null
+
+                if (isPreviewCircle) {
+                    srcPaint?.let { canvas.drawCircle(height / 2f, height / 2f, height / 2f, it) }
+                    canvas.drawCircle(height / 2f, height / 2f, height / 2f, paint!!)
+                } else {
+                    srcPaint?.let { canvas.drawRoundRect(rectF!!, canvasRadius, canvasRadius, it) }
+                    canvas.drawRoundRect(rectF!!, canvasRadius, canvasRadius, paint!!)
+                }
+            }
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        Log.d("RainbowColorBarView", "onSizeChanged ")
+        if (canvasType == 3 && isPreviewCircle && oldw != oldh) {
+            val lp = layoutParams
+            lp.width = lp.height
+            layoutParams = lp
+            Log.d("RainbowColorBarView", "onSizeChanged 1")
+        }
+        rectF?.set(0f, 0f, width.toFloat(), height.toFloat())
+    }
+
+}
